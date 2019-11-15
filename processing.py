b900899dc2b82740ba980103685d72a5a391875f
alake16 <aclake@umich.edu>
2019-11-14 18:32:43 -0600

    Implemented grid search and predicting on test data and logging metrics

--- processing.py
+++ processing.py
@@ -5,12 +5,13 @@ from random import seed
 from random import randrange
 from sklearn.model_selection import GridSearchCV
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 from sklearn.model_selection import cross_val_score
 # import warnings filter
 from warnings import simplefilter
 # ignore all future warnings
 simplefilter(action='ignore', category=FutureWarning)
-from sklearn.model_selection import KFold
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 def fetch_reviews(filename, sentiment):
 	reviews = []
@@ -68,10 +69,8 @@ def map_reviews_to_array(reviews, feature_vector):
 
 def cross_validation_split(data, folds=3):
 	seed(1)
-	print(len(data))
 	dataset_split = []
 	dataset_copy = list(data)
-	print(len(dataset_copy))
 	fold_size = len(data) // folds
 	# print(fold_size)
 	for i in range(folds):
@@ -85,15 +84,12 @@ def cross_validation_split(data, folds=3):
 		dataset_split.append(np.array(fold))
 	return np.array(dataset_split)
 
-def n_fold_cross_validation(X, y, split):
+def n_fold_cross_validation(X, y, model, split):
 	scores = []
-	model = LogisticRegression()
-	cv = KFold(n_splits=10, random_state=42, shuffle=False)
-	print(type(split))
-	print(split)
 	i = 0
 	for test_index in split:
 		train_list = []
+		print("Split {}/{}".format(i + 1, len(split)))
 		for j in range(0, len(split)):
 			if (i != j):
 				train_list.append(split[j])
@@ -102,16 +98,43 @@ def n_fold_cross_validation(X, y, split):
 		X_train, X_test, y_train, y_test = X.iloc[train_index], X.iloc[test_index], y.iloc[train_index], y.iloc[test_index]
 		model.fit(X_train, y_train.values.ravel())
 		scores.append(model.score(X_test, y_test))
-	print(np.mean(scores))
-	return model
+	return np.mean(scores)
+
+def grid_serach(X_train, y_train, split):
+	penalty = ['l1', 'l2']
+	c = [1.0, 0.1, 0.01, 0.001, 0.0001]
+	param_grid = dict(penalty=penalty, c=c)
+	best_score = 0
+	best_model = LogisticRegression()
+	for penalty_val in penalty:
+		for c_val in c:
+			model = LogisticRegression(penalty=penalty_val, C=c_val)
+			score = n_fold_cross_validation(X_train, y_train, model, split)
+			print("Cross validation score using {} penalty and C val of {}: {}".format(penalty_val, c_val, score))
+			if (score > best_score):
+				best_model = model
+	return best_model
+
+def predict_test_data(model, X_test, y_test):
+	preds = model.predict(X_test)
+	acc_score = accuracy_score(y_test, preds)
+	print("Model accuracy score on test data: {}".format(acc_score))
+	prec_score = precision_score(y_test, preds)
+	print("Model precision score on test data: {}".format(prec_score))
+	rec_score = recall_score(y_test, preds)
+	print("Model recall score on test data: {}".format(rec_score))
+	f1_sco = f1_score(y_test, preds)
+	print("Model f1 score on test data: {}".format(f1_sco))
 
 X_train, X_dev, X_test, y_train, y_dev, y_test = parse_dataset()
 feature_vector = create_feature_vector(X_train["review"], 5, 5000)
 X_train = map_reviews_to_array(X_train, feature_vector)
+X_test = map_reviews_to_array(X_test, feature_vector)
 cols = {"sentiment": y_train}
 y_train = pd.DataFrame(cols)
 X_train.reset_index(drop=True, inplace=True)
 y_train.reset_index(drop=True, inplace=True)
 df = pd.concat([X_train, y_train], axis=1)
-split = cross_validation_split(df.values.tolist())
-n_fold_cross_validation(X_train, y_train, split)
+split = cross_validation_split(df.values.tolist(), 10)
+model = grid_serach(X_train, y_train, split)
+predict_test_data(model, X_test, y_test)
